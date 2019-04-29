ed47430c2e709369baeca411c3aa1efd533ca74e
Samantha Lee <samanthal.lee3@gmail.com>
2019-04-29 00:50:23 +0000

    Working user page to edit places information

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
   def index
-    @places = Place.all.paginate(page: params[:page], per_page: 2)
+    @places = Place.all.paginate(page: params[:page], per_page: 4)
   end
 
   def new
@@ -21,6 +21,13 @@ class PlacesController < ApplicationController
   def edit
     @place = Place.find(params[:id])
   end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
 
 
   private
