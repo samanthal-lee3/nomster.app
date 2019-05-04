0f81e33ae9cbb60371d93eb9c65c198d05a0b1e9
Samantha Lee <samanthal.lee3@gmail.com>
2019-05-04 15:48:52 +0000

    comment controller working- adding comment to db connected to place and user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,18 +3,13 @@ class CommentsController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.comments.create(comment_params.merge(user: current_user)
+    @place.comments.create(comment_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
-
-
-
-
 
   private
 
   def comment_params
     params.require(:comment).permit(:message, :rating)
   end
-
 end
