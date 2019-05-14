698af1f7acda7f638821a8bce1a18afda69e25f2
Samantha Lee <samanthal.lee3@gmail.com>
2019-05-14 01:56:30 +0000

    working on emails sending to place owners

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@westdenvereats.com"
 
-  def comment_added
-    mail(to: "samanthal.lee3@gmail.com",
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+
+    mail(to: @place_owner.email,
       subject: "A comment has been added to your place")
   end
 
