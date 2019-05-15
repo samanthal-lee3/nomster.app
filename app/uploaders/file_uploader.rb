8c8e82e9e72b7bcec3a5cf1339a7d9776d950f29
Samantha Lee <samanthal.lee3@gmail.com>
2019-05-15 03:33:20 +0000

    updtated readme file with demo login and heroku link

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -9,9 +9,10 @@ class FileUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def cache_dir
+def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
